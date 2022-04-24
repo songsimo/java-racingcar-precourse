ff6f79b060382a2f09e78d18d7ccccdb8853955a
songsimo <chy.song89@gmail.com>
2022-04-24 15:06:24 +0900

    feat: 이름 유효성 검증 구현

---
+++ src/main/java/racingcar/utils/Error.java
@@ -0,0 +1,19 @@
+package racingcar.utils;
+
+public enum Error {
+	INVALID_TEXT_NAME("[ERROR] 유효하지 문자(공백)가 포함되어 있습니다."),
+	INVALID_NAME_LENGTH("[ERROR] 유효하지 문자(공백)가 포함되어 있습니다.")
+	;
+
+	public static final String COMMON_MESSAGE = "[ERROR]";
+
+	private String message;
+
+	Error(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return COMMON_MESSAGE + " " +message;
+	}
+}
