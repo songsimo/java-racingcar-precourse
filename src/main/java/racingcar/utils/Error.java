bbf8dfb65b8f9fa49f38ce5f056d442ec9463fca
songsimo <chy.song89@gmail.com>
2022-04-25 22:22:29 +0900

    fix: 에러 메세지 '[ERROR]' 중복 기입 수정

--- src/main/java/racingcar/utils/Error.java
+++ src/main/java/racingcar/utils/Error.java
@@ -1,9 +1,9 @@
 package racingcar.utils;
 
 public enum Error {
-	INVALID_TEXT_NAME("[ERROR] 유효하지 문자(공백)가 포함되어 있습니다."),
-	INVALID_NAME_LENGTH("[ERROR] 유효하지 문자(공백)가 포함되어 있습니다."),
-	INVALID_NUMBER("[ERROR] 시도 횟수는 숫자여야 한다.")
+	INVALID_TEXT_NAME("유효하지 문자(공백)가 포함되어 있습니다."),
+	INVALID_NAME_LENGTH("이름은 최소 1글자에서 최대 5글자까지 가능합니다."),
+	INVALID_NUMBER("시도 횟수는 숫자여야 한다.")
 	;
 
 	public static final String COMMON_MESSAGE = "[ERROR]";
